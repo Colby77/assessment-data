dcf22db18914c4ac64d77d63e7b1061a651a71aa
Colby Workman <cgw2319@gmail.com>
2021-11-12 09:21:37 -0700

    process.py done

--- process.py
+++ process.py
@@ -5,7 +5,7 @@ def sales_reports(log_file):    # defines a fuction taking in a file
     for line in log_file:       # for each line in the file it strips it of whitespace
         line = line.rstrip()    #  then pulls the day of the line and if the day
         day = line[0:3]         # and if the day is tuesday it will print the line
-        if day == "Tue":
+        if day == "Mon":
             print(line)
 
 
